c64fcdc9fdae62a8d466f074bc1b419eead2803f
SarahBurman <saraburman96@gmail.com>
2023-06-29 12:31:30 +0300

    started working on tree ui

--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -1,7 +1,9 @@
+import { NestedTreeControl } from '@angular/cdk/tree';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
+import { Subject, takeUntil } from 'rxjs';
 import { ServerService } from '../server.service';
-import { Directory } from '../utils';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { DirectoryNode, convertToTree } from '../utils';
 @Component({
   selector: 'tree-tree',
   templateUrl: './tree.component.html',
@@ -9,14 +11,20 @@ import { Observable, Subject, takeUntil } from 'rxjs';
 })
 export class TreeComponent implements OnInit, OnDestroy {
 
-  rootDirectory$: Observable<Directory> | undefined;
   private destroy$: Subject<void> = new Subject<void>();
 
+  dataSource = new MatTreeNestedDataSource<DirectoryNode>();
+  nestedTree = new NestedTreeControl<DirectoryNode>(node => node.children);
 
-  constructor(private serverService:ServerService){}
+  constructor(private serverService:ServerService){
+  }
 
   ngOnInit() {
-    this.rootDirectory$ = this.serverService.getData().pipe(takeUntil(this.destroy$));
+    this.serverService.getData().pipe(takeUntil(this.destroy$)).subscribe(tree => this.dataSource.data = convertToTree(tree));
+  }
+
+  hasNestedChild(index:number, node:DirectoryNode): boolean{
+    return (!!node.files && node.files.length > 0) || (!!node.children && node.children.length > 0);
   }
 
   ngOnDestroy(): void {
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -6,13 +6,14 @@ export interface Directory {
 
 export interface DirectoryNode {
     name: string;
+    files?:string[];
     children?: DirectoryNode[];
 }
 
-export function convertToTree(data: Directory): DirectoryNode{
+export function convertToTree(data: Directory[]): DirectoryNode[] {
   
     const createNode = (directory: Directory): DirectoryNode => {
-      const node: DirectoryNode = { name: directory.name, children: [] };
+      const node: DirectoryNode = { name: directory.name,files:directory.files, children: [] };
   
       if (directory.directories) {
         directory.directories.forEach(child => {
@@ -21,15 +22,15 @@ export function convertToTree(data: Directory): DirectoryNode{
         });
       }
   
-      if (directory.files && directory.files.length > 0) {
-        // Add file nodes under the parent node (optional, based on your requirement)
-        node.children?.push(...directory.files.map(file => ({ name: file })));
-      }
+      // if (directory.files && directory.files.length > 0) {
+      //   // Add file nodes under the parent node (optional, based on your requirement)
+      //   node.children?.push(...directory.files.map(file => ({ name: file })));
+      // }
   
       return node;
     };
   
-    const treeData = createNode(data);
-  
+    // const treeData = createNode(data);
+    const treeData = data.map(directory => createNode(directory));
     return treeData;
   };
